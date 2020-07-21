3eeea6e3767a86a47d402ba2cdf45e2861f8eca6
yujunxia <yujunxia@allwinnertech.com>
2020-07-21 15:15:53 +0800

    sd: adapt sd api
    Change-Id: I9cea8eb84608c37fd0cda2c306d6bdfa6b169a6e

--- platform/mcu/xr872/hal/soc/sd.c
+++ platform/mcu/xr872/hal/soc/sd.c
@@ -27,15 +27,125 @@
  *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <stdio.h>
 #include <sys/unistd.h>
 #include "aos/hal/sd.h"
 #include "driver/chip/hal_def.h"
 #include "driver/chip/sdmmc/hal_sdhost.h"
 #include "driver/chip/sdmmc/sdmmc.h"
 
-static struct mmc_card card;
+#define SD_DBG_ON	0
+#define SD_INF_ON	0
+#define SD_WRN_ON	1
+#define SD_ERR_ON	1
+
+#define SD_LOG(flags, fmt, arg...)  \
+    do {                            \
+        if (flags)                  \
+            printf(fmt, ##arg);     \
+    } while (0)
+
+#define SD_DBG(fmt, arg...) SD_LOG(SD_DBG_ON, "[SD DBG] "fmt, ##arg)
+#define SD_INF(fmt, arg...) SD_LOG(SD_INF_ON, "[SD INF] "fmt, ##arg)
+#define SD_WRN(fmt, arg...) SD_LOG(SD_WRN_ON, "[SD WRN] "fmt, ##arg)
+#define SD_ERR(fmt, arg...)                         \
+    do {                                            \
+        SD_LOG(SD_ERR_ON, "[SD ERR] %s():%d, "fmt,  \
+               __func__, __LINE__, ##arg);          \
+    } while (0)
+
+#define SDC_ID	0
+
 static hal_sd_info_t sd_info;
 static int sd_initialize = 0;
+
+#ifdef CONFIG_DETECT_CARD
+static void card_detect(uint32_t present)
+{
+	if (present) {
+		SD_DBG("card exist\n");
+	} else {
+		SD_WRN("card not exist\n");
+	}
+}
+#endif
+
+static int sd_init()
+{
+	struct mmc_host *host;
+	SDC_InitTypeDef sdc_param;
+
+	SD_DBG("%s %d\n", __func__, __LINE__);
+
+#ifdef CONFIG_DETECT_CARD
+	sdc_param.cd_mode = PRJCONF_MMC_DETECT_MODE;
+	sdc_param.cd_cb = card_detect;
+#endif
+	sdc_param.debug_mask = 0x0;
+	sdc_param.dma_use = 1;
+	host = HAL_SDC_Create(SDC_ID, &sdc_param);
+	if (host == NULL) {
+		SD_ERR("sdc create fail\n");
+		return -1;
+	}
+	if (HAL_SDC_Init(host) == NULL) {
+		SD_ERR("sdc init fail\n");
+		return -1;
+	}
+	return 0;
+}
+
+static int sd_deinit()
+{
+	struct mmc_card *card = mmc_card_open(SDC_ID);
+	if (card == NULL) {
+		SD_ERR("card open fail\n");
+		return -1;
+	} else {
+		if (mmc_card_present(card)) {
+			mmc_card_deinit(card);
+		}
+		mmc_card_close(SDC_ID);
+		mmc_card_delete(SDC_ID);
+	}
+	HAL_SDC_Deinit(SDC_ID);
+	HAL_SDC_Destory(card->host);
+
+	return 0;
+}
+
+static struct mmc_card *sd_card_scan()
+{
+	struct mmc_card *card;
+
+	SDCard_InitTypeDef card_param = {0};
+	card_param.debug_mask = 0x0;
+	card_param.type = 2;
+	if (mmc_card_create(SDC_ID, &card_param) != 0) {
+		SD_ERR("mmc create fail\n");
+		return NULL;
+	}
+	card = mmc_card_open(SDC_ID);
+	if (card == NULL) {
+		SD_ERR("mmc open fail\n");
+		mmc_card_delete(SDC_ID);
+		return NULL;
+	}
+	if (!mmc_card_present(card)) {
+		int mmc_ret = mmc_rescan(card, SDC_ID);
+		if (mmc_ret != 0) {
+			SD_ERR("mmc scan fail\n");
+			mmc_card_close(SDC_ID);
+			mmc_card_delete(SDC_ID);
+			return NULL;
+		} else {
+			SD_INF("mmc init\n");
+		}
+	}
+	mmc_card_close(SDC_ID);
+
+	return card;
+}
 
 /**@brief Initialises a sd interface
 *
@@ -48,31 +158,32 @@ static int sd_initialize = 0;
 int32_t hal_sd_init(sd_dev_t *sd)
 {
 	int32_t ret;
-	SDC_InitTypeDef sdc_param = {0};
+	struct mmc_card *card = NULL;
+
+	SD_DBG("%s %d\n", __func__, __LINE__);
 
 	if (!sd_initialize) {
-		memset(&card, 0, sizeof(struct mmc_card));
 		memset(&sd_info, 0, sizeof(hal_sd_info_t));
 
-		sd->port = 0;
-		//HAL_SDC_Init(sd->port, &sdc_param);
-
-		//ret = mmc_rescan(&card, sd->port);
-		if (ret != 0) {
-			printf("[hal_sd]: init sd failed\n");
+		ret = sd_init();
+		if (ret != 0)
 			return -1;
-		}
+
+		card = sd_card_scan();
+		if (card == NULL)
+			return -1;
 	    sd_initialize = 1;
 	}
 
-	sd->config.bus_wide = card.bus_width;
-	if (card.state & MMC_STATE_HIGHSPEED)
+	sd->port = SDC_ID;
+	sd->config.bus_wide = card->bus_width;
+	if (card->state & MMC_STATE_HIGHSPEED)
 		sd->config.freq = 50000000;
 	else
 		sd->config.freq = 25000000;
-	sd->priv = (void*)&card;
+	sd->priv = (void*)card;
 	sd_info.blk_size = 512;
-	sd_info.blk_nums = card.csd.capacity/512;
+	sd_info.blk_nums = card->csd.capacity/512;
 
 	return 0;
 }
@@ -90,9 +201,9 @@ int32_t hal_sd_init(sd_dev_t *sd)
 */
 int32_t hal_sd_blks_read(sd_dev_t *sd, uint8_t *data, uint32_t blk_addr, uint32_t blks, uint32_t timeout)
 {
-	int32_t ret;
-	//ret = mmc_block_read(sd->priv, data, blk_addr, blks);
-
+	int32_t ret = -1;
+	if (sd_initialize && sd->priv)
+		ret = mmc_block_read(sd->priv, data, blk_addr, blks);
 	return ret;
 }
 
@@ -108,9 +219,9 @@ int32_t hal_sd_blks_read(sd_dev_t *sd, uint8_t *data, uint32_t blk_addr, uint32_
 */
 int32_t hal_sd_blks_write(sd_dev_t *sd, uint8_t *data, uint32_t blk_addr, uint32_t blks, uint32_t timeout)
 {
-	int32_t ret;
-	//ret = mmc_block_write(sd->priv, data, blk_addr, blks);
-
+	int32_t ret = -1;
+	if (sd_initialize && sd->priv)
+		ret = mmc_block_write(sd->priv, data, blk_addr, blks);
 	return ret;
 }
 
@@ -163,11 +274,48 @@ int32_t hal_sd_info_get(sd_dev_t *sd, hal_sd_info_t *info)
 */
 int32_t hal_sd_finalize(sd_dev_t *sd)
 {
-	sd_initialize = 0;
-	if (!card.host)
+	if (!sd_initialize)
 		return 0;
+	sd_initialize = 0;
 
-	mmc_card_deinit(&card);
-	HAL_SDC_Deinit(0);
-	return 0;
+	return sd_deinit();
 }
+
+#if 0
+void sd_test()
+{
+	int i, ret;
+	static sd_dev_t sdmmc;
+	uint8_t read_buff[1024] = {0};
+	uint8_t write_buff[1024];
+
+	for (i = 0; i < 1024; i++) {
+		write_buff[i] = (uint8_t)i;
+		//printf("write: %d\n", write_buff[i]);
+	}
+
+	if (hal_sd_init(&sdmmc) != 0) {
+		printf("hal_sd_init fail\n");
+		return;
+	}
+
+	ret = hal_sd_blks_write(&sdmmc, write_buff, 2000, 2, 1);
+	if (ret != 0) {
+		printf("%s write fail\n", __func__);
+	} else
+		printf("%s write success\n", __func__);
+
+	ret = hal_sd_blks_read(&sdmmc, read_buff, 2000, 2, 1);
+	if (ret != 0) {
+		printf("%s read fail\n", __func__);
+	} else
+		printf("%s read success\n", __func__);
+
+	for(i=0; i < 1024; i++)
+		printf("read: %d\n", read_buff[i]);
+
+	hal_sd_finalize(&sdmmc);
+
+	printf("test end------------\n");
+}
+#endif
